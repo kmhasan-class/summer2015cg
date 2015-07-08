c38e90d118b54dc44d119a264109c326718a8dcf
kmhasan <kmhasan@gmail.com>
2015-07-09 01:58:49 +0600

    depth test fails for 3d viewing

--- OGL 3D Viewing/main.cpp
+++ OGL 3D Viewing/main.cpp
@@ -13,9 +13,23 @@ void reshape(int width, int height) {
     glLoadIdentity();
 }
 
+void setMaterial(float a[], float d[], float s[], float f[]) {
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, a);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, d);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, s);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, f);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+}
+
 void display() {
     theta += 0.1;
-    glClear(GL_COLOR_BUFFER_BIT);
+    float ambient[] = {0.1, 0.1, 0.0};
+    float diffuse[] = {0.4, 0.4, 0.1};
+    float specular[] = {0.8, 0.8, 0.8};
+    float shininess[] = {50};
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluPerspective(45, aspectRatio, 0.001, 100.0);
@@ -25,6 +39,7 @@ void display() {
     glPushMatrix();
         glColor3f(1.0, 1.0, 0.0);
         glRotatef(theta, 0, 1, 0);
+        setMaterial(ambient, diffuse, specular, shininess);
         glutSolidTeapot(3.0);
     glPopMatrix();
     glFlush();
